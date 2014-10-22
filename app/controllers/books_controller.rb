2b2f51bea1ea706f19eb3990ed1ca248c614997e
Andrei Cirnici <a.cirnici@gmail.com>
2014-10-22 13:30:12 +0300

    Update method added; new, edit vies refactored

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,21 +1,46 @@
 class BooksController < ApplicationController
 
   def new
+    @book = Book.new
+  end
 
+  def index
+    @books = Book.all
   end
 
   def create
     @book = Book.new(book_params)
 
-    @book.save
-    redirect_to @book
+    if @book.save
+      redirect_to @book
+    else
+      render "new"
+    end
 
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+
+    if @book.update(book_params)
+      redirect_to @book
+    else
+      render "edit"
+    end
+  end
+
+  def show
+    @book = Book.find(params[:id])
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title, :year, :author, :description)
+    params.require(:book).permit(:title, :author, :year, :description)
   end
 
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -2,8 +2,8 @@ class Book < ActiveRecord::Base
 
   validates :title, :author, :year, presence: true
   validates :title, uniqueness: { scope: :year,
-                                      message: "There can be only one issue a year",
-                                      case_sensitive: false }
+                                  message: "There can be only one issue a year",
+                                  case_sensitive: false }
 
 
 end
