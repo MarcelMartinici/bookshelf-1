3b95b38dd303f8854c8cdc97f051f9c85828fae1
Andrei Cirnici <a.cirnici@gmail.com>
2014-10-24 12:08:23 +0300

    User roles Added

--- Gemfile
+++ Gemfile
@@ -26,7 +26,11 @@ gem 'sdoc', '~> 0.4.0',          group: :doc
 # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
 gem 'spring',        group: :development
 
+# User sign_up
 gem 'devise'
+
+# User roles
+gem 'cancancan', '~> 1.9'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,19 @@
 class User < ActiveRecord::Base
+
   # Include default devise modules. Others available are:
   #  :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :books
+
+  attr_accessor :name, :email
+
+  # User::Roles
+  # The available roles
+  Roles = [:admin, :default]
+
+  def is?(requested_role)
+    self.role == requested_role.to_s
+  end
 end
